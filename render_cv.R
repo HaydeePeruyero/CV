40cb71d14f626a8e326687be6bea327b6e192124
HaydeePeruyero <haydeeperuyero@gmail.com>
2025-08-05 11:30:58 -0600

    Update extended version

--- render_cv.R
+++ render_cv.R
@@ -11,7 +11,7 @@
 library(tidyverse)
 source("cv_printing_functions.R")
 cv_data <- create_CV_object(
-  data_location = "https://docs.google.com/spreadsheets/d/1wRtEfO0mqAP3UdmZDiNqLEBJ4V4Cekw0VfN8O-IWi-8/edit?usp=sharing"
+  data_location = "https://docs.google.com/spreadsheets/d/1yUxdL00yte1czYjmBTptsGtzu5qH6MX4Mdf-eHRUzcc/edit?usp=sharing"
 )
 
 readr::write_rds(cv_data, 'cached_positions.rds')
